c241e04811aebf6c0a0dedefc243281a87b24be7
Amicabl <64450327+Amicabl@users.noreply.github.com>
2024-02-11 18:54:44 -0800

    Update ArrayTests.java

--- ArrayTests.java
+++ ArrayTests.java
@@ -3,11 +3,19 @@ import org.junit.*;
 
 public class ArrayTests {
 	@Test 
-	public void testReverseInPlace() {
-    int[] input1 = { 3 };
+	public void testReverseInPlace1() {
+    int[] input1 = {3};
     ArrayExamples.reverseInPlace(input1);
     assertArrayEquals(new int[]{ 3 }, input1);
 	}
+
+  @Test 
+	public void testReverseInPlace2() {
+    int[] input1 = {1, 2, 3};
+    ArrayExamples.reverseInPlace(input1);
+    assertArrayEquals(new int[]{3, 2, 1}, input1);
+	}
+
 
 
   @Test
